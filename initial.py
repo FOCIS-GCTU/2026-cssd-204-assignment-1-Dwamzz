245ad71ea1033dcad5babf874a93370493285254
Dwamzz <2425402774@live.gctu.edu.gh>
2026-05-31 00:03:41 +0000

    Add files via upload

---
+++ initial.py
@@ -0,0 +1,26 @@
+def main():
+
+    print("...KDO")
+
+print("...KKK......KKK.........DDDDDDDD............OOOOOOOOOOOO")
+print("...KKK.....KKK..........DDDDDDDDD...........OOOOOOOOOOOO")
+print("...KKK....KKK...........DD......DDD.........OOO......OOO")
+print("...KKK...KKK............DD.......DDD........OOO......OOO")
+print("...KKK.KKK..............DD.......DDD........OOO......OOO")
+print("...KKK..KKK.............DD.......DDD........OOO......OOO")
+print("...KKK...KKK............DD.......DDD........OOO......OOO")
+print("...KKK....KKK...........DD.......DDD........OOO......OOO")
+print("...KKK.....KKK.....**...DDDDDDDDDD.....**...OOOOOOOOOOOO")
+print("...KKK......KKK....**...DDDDDDDD.......**...OOOOOOOOOOOO")
+
+#File:initial.py
+# Description: Print out my initials.
+# Assignment Number: 1
+
+# Name: Kwame Dwamena Oware
+# STUDENT ID:2425402774
+# Email: 2425402774@gctu.live.edu.gh
+# Grader: Augustus
+#
+# On my honor,Kwame Dwamena Oware , this programming assignment is my own work
+# and I have not provided this code to any other student.
